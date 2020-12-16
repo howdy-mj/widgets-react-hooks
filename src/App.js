a1e8fccc08d390a8e24df36c29337e7feef6dc26
kmj <hi.minjungkim@gmail.com>
2020-12-16 22:25:27 +0900

    add: styling and items of Accordion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a frontend javascript framework',
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library amon engineers',
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components',
+  },
+];
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <br />
+      <Accordion items={items} />
+    </div>
+  );
 };
 
 export default App;
